1d02c7a9ec1fcb7699833102ab9e497074c1082e
Paloma Maquez <maquez.paloma@gmail.com>
2021-12-11 16:28:26 -0300

    animation

--- jquery.js
+++ jquery.js
@@ -1,19 +1,20 @@
-$('input').focus(function(){
-  $(this).css('background','#d3d3d3');
-});
-$('textarea').focus(function(){
-  $(this).css('background','#d3d3d3');
-});
-$('input').blur(function(){
-  $(this).css('background','white');
-});
 $('textarea').blur(function(){
   $(this).css('background','white');
 });
 $('#form').submit(function(e){
   e.preventDefault();
-  alert('Enviado Exitosamente');
+  // $('#form').text('Enviado Exitosamente');
 });
 $('h5').text('Contacto');
 $('.info-contact').html('Escribinos cualquier duda que tengas!');
 $('.info-contact').css('color', 'black');
+//Animations
+$(document).ready(function(){
+  $("#input-submit").click(function(){
+    $("#input-submit").animate({
+      width: '600px',
+      height: '100px',
+    }, 1000,);
+    $(".input-clickText").show().animate({opacity: '1'}, 2000);
+  });
+});
